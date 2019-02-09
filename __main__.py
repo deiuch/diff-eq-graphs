7fa98e0c1d9925e3732db754cac2f793c82ddffe
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2019-02-09 21:11:40 +0300

    `__main__.py` imports fixed

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
-import main
 import modules_inst
 
 # Install all required modules if they are not installed
 modules_inst.install('PyQt5', 'pyqtgraph')
 
-main.main()
+from main import main
+
+main()
