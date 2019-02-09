14e134f876c2ea12fb3f0ea874d1ed8b0b994fa0
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2019-02-09 21:11:23 +0300

    `pip` interface usage updated

--- modules_inst.py
+++ modules_inst.py
@@ -1,7 +1,7 @@
-import pip
-import importlib
+from pip._internal import main
+from importlib.util import find_spec
 
-NOT_FOUND_MSG = 'Module {} was not found! Try to install it? [y/n] '
+NOT_FOUND_MSG = 'Module {} was not found! Try to install it? [Y/n] '
 NOT_INSTALLED_MSG = 'Module {} wan unable to be installed! Try again later!'
 
 
@@ -12,9 +12,9 @@ def install(*module_names):
     :return If everything is installed - return 0, else - 1
     """
     for m_name in module_names:
-        if not importlib.util.find_spec(m_name):
-            if input(NOT_FOUND_MSG.format(m_name)).lower() in 'y':
-                if pip.main(['install', m_name]):
+        if not find_spec(m_name):
+            if input(NOT_FOUND_MSG.format(m_name)).lower() not in 'Nn':
+                if main(['install', m_name]):
                     print(NOT_INSTALLED_MSG.format(m_name))
                     return 1
             else:
